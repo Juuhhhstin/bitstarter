fad3fd2dd746e1fdaa7746e5b540f3db2b0b94b6
Juuhhhstin <justin.kj.chang@gmail.com>
2013-07-12 17:13:19 +0000

    edited web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html', 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
